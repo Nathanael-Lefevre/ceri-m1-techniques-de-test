0fa477ade065acde1714a99e93fdeeb9afd0c58b
Sierra-Springs <nathanael-1999@hotmail.fr>
2022-04-28 13:34:45 +0200

    TP1 : Fork them all ! setup du Lifecycle fin

---
+++ src/test/java/fr/univavignon/pokedex/api/TestMain.java
@@ -0,0 +1,10 @@
+package fr.univavignon.pokedex.api;
+
+import junit.framework.TestCase;
+
+public class TestMain extends TestCase {
+
+    public void testToSee() {
+        assertEquals(2, 1 + 1);
+    }
+}
