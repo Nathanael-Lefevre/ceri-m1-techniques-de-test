081d7a2c7f98c57a45eb6f8b87dbbb24ea9087a5
Sierra-Springs <nathanael-1999@hotmail.fr>
2022-05-02 18:40:40 +0200

    suppression des classes inutiles

--- src/main/java/fr/univavignon/pokedex/api/Main.java
+++
@@ -1,7 +0,0 @@
-package fr.univavignon.pokedex.api;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("first statement.");
-    }
-}
--- src/test/java/fr/univavignon/pokedex/api/TestMain.java
+++
@@ -1,9 +0,0 @@
-package fr.univavignon.pokedex.api;
-
-import junit.framework.TestCase;
-
-public class TestMain extends TestCase {
-    public void testToSee() {
-        assertEquals(2, 1 + 1);
-    }
-}
