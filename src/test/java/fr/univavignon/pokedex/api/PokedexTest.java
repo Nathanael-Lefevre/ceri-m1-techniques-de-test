81594225d8ffcdea7c3feb6491d6440866736670
Sierra-Springs <nathanael-1999@hotmail.fr>
2022-05-03 16:44:07 +0200

    Correction d'un test qui ne marcherait plus sans le mock

--- src/test/java/fr/univavignon/pokedex/api/PokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/PokedexTest.java
@@ -22,7 +22,7 @@ public class PokedexTest implements IPokedexTest {
     @Override
     @Before
     public void init() throws PokedexException {
-        mPokemonList = new ArrayList<Pokemon>();
+        mPokemonList = new ArrayList<>();
         pokemon0 = new Pokemon(0, "Bulbizarre", 126, 126, 90, 613, 64, 4000, 4, .56);
         pokemon1 = new Pokemon(133, "Aquali", 186, 168, 260, 2729, 202, 5000, 4, 1.);
         pokemon2 = new Pokemon(6, "Carapuce", 44, 65, 0, 3310, 202, 5000, 4, 1.);
@@ -30,63 +30,45 @@ public class PokedexTest implements IPokedexTest {
         mPokedex = Mockito.mock(IPokedex.class);
 
         when(mPokedex.size()).thenAnswer(
-                new Answer<Integer>() {
-                    public Integer answer(InvocationOnMock invocation) {
-                        return mPokemonList.size();
-                    }
-                });
+                (Answer<Integer>) invocation -> mPokemonList.size());
 
         when(mPokedex.addPokemon(any(Pokemon.class))).then(
-                new Answer<>() {
-            public Object answer(InvocationOnMock invocation) {
-                Pokemon p = invocation.getArgument(0);
-                mPokemonList.add(p);
-                return mPokemonList.indexOf(p);
-            }
-        });
+                invocation -> {
+                    Pokemon p = invocation.getArgument(0);
+                    mPokemonList.add(p);
+                    return mPokemonList.indexOf(p);
+                });
 
         when(mPokedex.getPokemon(any(Integer.class))).then(
-                new Answer<>() {
-                    public Pokemon answer(InvocationOnMock invocation) throws PokedexException {
-                        Integer index = invocation.getArgument(0);
-                        if (index < 0) {
-                            throw new PokedexException("L'index du Pokemon dans le pokedex doit être supérieur à -1");
-                        } else if (index > (mPokemonList.size() - 1)) {
-                            throw new PokedexException("Le Pokedex ne contient pas autant de pokemon !");
-                        }
-                        return mPokemonList.get(index);
+                invocation -> {
+                    Integer index = invocation.getArgument(0);
+                    if (index < 0) {
+                        throw new PokedexException("L'index du Pokemon dans le pokedex doit être supérieur à -1");
+                    } else if (index > (mPokemonList.size() - 1)) {
+                        throw new PokedexException("Le Pokedex ne contient pas autant de pokemon !");
                     }
+                    return mPokemonList.get(index);
                 });
 
         when(mPokedex.getPokemons()).then(
-                new Answer<List<Pokemon>>() {
-                    public List<Pokemon> answer(InvocationOnMock invocation) {
-                        return Collections.unmodifiableList(mPokemonList);
-                    }
-                });
+                (Answer<List<Pokemon>>) invocation -> Collections.unmodifiableList(mPokemonList));
 
         when(mPokedex.getPokemons(PokemonComparators.CP)).then(
-                new Answer<List<Pokemon>>() {
-                    public List<Pokemon> answer(InvocationOnMock invocation) {
-                        List<Pokemon> sortedCPList = new ArrayList<>(Arrays.asList(pokemon0, pokemon1, pokemon2));
-                        return Collections.unmodifiableList(sortedCPList);
-                    }
+                (Answer<List<Pokemon>>) invocation -> {
+                    List<Pokemon> sortedCPList = new ArrayList<>(Arrays.asList(pokemon0, pokemon1, pokemon2));
+                    return Collections.unmodifiableList(sortedCPList);
                 });
 
         when(mPokedex.getPokemons(PokemonComparators.INDEX)).then(
-                new Answer<List<Pokemon>>() {
-                    public List<Pokemon> answer(InvocationOnMock invocation) {
-                        List<Pokemon> sortedIndexList = new ArrayList<>(Arrays.asList(pokemon0, pokemon2, pokemon1));
-                        return Collections.unmodifiableList(sortedIndexList);
-                    }
+                (Answer<List<Pokemon>>) invocation -> {
+                    List<Pokemon> sortedIndexList = new ArrayList<>(Arrays.asList(pokemon0, pokemon2, pokemon1));
+                    return Collections.unmodifiableList(sortedIndexList);
                 });
 
         when(mPokedex.getPokemons(PokemonComparators.NAME)).then(
-                new Answer<List<Pokemon>>() {
-                    public List<Pokemon> answer(InvocationOnMock invocation) {
-                        List<Pokemon> sortedNameList = new ArrayList<>(Arrays.asList(pokemon1, pokemon0, pokemon2));
-                        return Collections.unmodifiableList(sortedNameList);
-                    }
+                (Answer<List<Pokemon>>) invocation -> {
+                    List<Pokemon> sortedNameList = new ArrayList<>(Arrays.asList(pokemon1, pokemon0, pokemon2));
+                    return Collections.unmodifiableList(sortedNameList);
                 });
     }
 
@@ -100,7 +82,8 @@ public class PokedexTest implements IPokedexTest {
     @Test
     public void testShouldAddPokemonToPokedex() {
         mPokedex.addPokemon(pokemon0);
-        Assert.assertTrue(mPokemonList.contains(pokemon0));
+        List<Pokemon> actualPokemonList = mPokedex.getPokemons();
+        Assert.assertTrue(actualPokemonList.contains(pokemon0));
     }
 
     @Override
@@ -132,7 +115,7 @@ public class PokedexTest implements IPokedexTest {
 
     @Override
     @Test
-    public void testShouldThrowPokedexExceptionWhenIndexIsInvalid() throws PokedexException {
+    public void testShouldThrowPokedexExceptionWhenIndexIsInvalid() {
         Assert.assertThrows(PokedexException.class, () -> mPokedex.getPokemon(-2));  // not just -1
         Assert.assertThrows(PokedexException.class, () -> mPokedex.getPokemon(-1));
         Assert.assertThrows(PokedexException.class, () -> mPokedex.getPokemon(0));
